3a5da011973491a28e804d51185ed50dd2fe1901
jrdelrio <jrdelriodom@gmail.com>
2024-08-26 12:07:36 +0200

    proy dstacados

--- src/components/CardProyecto.js
+++ src/components/CardProyecto.js
@@ -2,16 +2,18 @@
 
 const CardProyecto = (props) => {
 
-  return (
-    <div key={props.id} className="col-12 col-md-6 col-lg-3">
-      <div className="proy-dest-card text-start mb-4">
-        <img className="img-proy-dest" src={props.image} alt="alt" />
-        <h3 className="card-proyecto-title">{props.title}</h3>
-        <p className="card-proyecto-descr">{props.description}</p>
-        {/* <SecondaryButton /> */}
-      </div>
-    </div>
-  );
+	return (
+		<div key={props.id} className="col-12 col-md-6 col-lg-3">
+			<div className="proy-dest-card text-start mb-4">
+				<div className="zoom-wrapper">
+				<img className="img-proy-dest" src={props.image} alt="alt" />
+				</div>
+				<h3 className="card-proyecto-title">{props.title}</h3>
+				<p className="card-proyecto-descr">{props.description}</p>
+				{/* <SecondaryButton /> */}
+			</div>
+		</div>
+	);
 };
 
 export default CardProyecto;
